174766d98d11acfe2f808af7e00f1985e5b36775
haxianhe <haxianhe@kuaishou.com>
2024-01-26 13:22:52 +0800

    deploy

---
+++ assets/index.html-dJl8QEsL.js
@@ -0,0 +1,5 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as o,a as n,b as e,d as r}from"./app-UcIb3osA.js";const s={},c=e("h2",{id:"📮-联系我",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#📮-联系我","aria-hidden":"true"},"#"),r(" 📮 联系我")],-1),d=e("p",null,"haxianhe@gmail.com",-1);function h(i,m){return a(),o("div",null,[c,d,n(` 这是项目主页的案例。你可以在这里放置你的主体内容。
+
+想要使用此布局，你需要在页面 front matter 中设置 \`home: true\`。
+
+配置项的相关说明详见 [项目主页配置](https://theme-hope.vuejs.press/zh/guide/layout/home/)。 `)])}const u=t(s,[["render",h],["__file","index.html.vue"]]);export{u as default};
